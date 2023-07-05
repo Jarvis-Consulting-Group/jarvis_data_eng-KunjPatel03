dc79858ebd4761f1020cf80305515789d9588ef4
Kunj Patel <kunjvpatel9@gmail.com>
2023-07-05 15:09:50 +0000

    Implement OrderDAO

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -9,30 +9,35 @@ public class JDBCExecutor {
 
         try{
             Connection connection = dbConnectionManager.getConnection();
-            CustomerDAO customerDAO = new CustomerDAO(connection);
+
+//            CustomerDAO customerDAO = new CustomerDAO(connection);
 
 //            Insert data to customer
-            Customer customer = new Customer();
-            customer.setFirstName("George");
-            customer.setLastName("Washington");
-            customer.setEmail("george.washington@wh.gov");
-            customer.setPhone("(555) 555-1234");
-            customer.setAddress("1234 Main St");
-            customer.setCity("Mount Vernon");
-            customer.setState("VA");
-            customer.setZipCode("22121");
+//            Customer customer = new Customer();
+//            customer.setFirstName("George");
+//            customer.setLastName("Washington");
+//            customer.setEmail("george.washington@wh.gov");
+//            customer.setPhone("(555) 555-1234");
+//            customer.setAddress("1234 Main St");
+//            customer.setCity("Mount Vernon");
+//            customer.setState("VA");
+//            customer.setZipCode("22121");
 
 //            Update inserted customer
-            Customer dbCustomer = customerDAO.create(customer);
-            System.out.println(dbCustomer);
-            dbCustomer = customerDAO.findById(dbCustomer.getId());
-            System.out.println(dbCustomer);
-            dbCustomer.setEmail("george.washington@wh.gov");
-            dbCustomer = customerDAO.update(dbCustomer);
-            System.out.println(dbCustomer);
+//            Customer dbCustomer = customerDAO.create(customer);
+//            System.out.println(dbCustomer);
+//            dbCustomer = customerDAO.findById(dbCustomer.getId());
+//            System.out.println(dbCustomer);
+//            dbCustomer.setEmail("george.washington@wh.gov");
+//            dbCustomer = customerDAO.update(dbCustomer);
+//            System.out.println(dbCustomer);
 
 //            Delete last customer
-            customerDAO.delete(dbCustomer.getId());
+//            customerDAO.delete(dbCustomer.getId());
+
+            OrderDAO orderDAO = new OrderDAO(connection);
+            Order order = orderDAO.findById(1000);
+            System.out.println(order);
 
         }catch (SQLException e){
             e.printStackTrace();
